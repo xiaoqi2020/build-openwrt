c85366e997ee79724e158b14247f1ed3c6b5614a
xiaoqi2020 <50669177+xiaoqi2020@users.noreply.github.com>
2021-03-26 10:01:23 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -18,6 +18,3 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb packag
 svn co https://github.com/garypang13/openwrt-packages/trunk/chinadns-ng package/diycj/chinadns-ng
 svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-go package/diycj/trojan-go
 svn co https://github.com/garypang13/openwrt-packages/trunk/trojan-plus package/diycj/trojan-plus
-cd openwrt
-run: find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-run: find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
